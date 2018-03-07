2c2b5d26e0263b0edcac044dcc901d798e0f8a06
yuliang <yu.liang@freelog.com>
2018-03-07 11:16:25 +0800

    1.修改dockerfile

--- config/config.test.js
+++ config/config.test.js
@@ -4,13 +4,34 @@
 
 'use strict'
 
-module.exports = appInfo => {
-    return {
+module.exports = {
+    knex: {
+        //资源相关DB配置
+        resource: {
+            connection: {
+                host: 'rm-wz9wj9435a0428942.mysql.rds.aliyuncs.com',
+                user: 'freelog',
+                password: 'Ff@233109',
+                database: 'fr_resource',
+            },
+            debug: false
+        }
+    },
 
-        gatewayUrl: "http://192.168.164.129",
+    /**
+     * mongoDB配置
+     */
+    mongoose: {
+        url: "mongodb://root:Ff233109@dds-wz9b5420c30a27941546-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9b5420c30a27942267-pub.mongodb.rds.aliyuncs.com:3717/resource?replicaSet=mgset-5016983"
+    },
 
-        mongoose: {
-            url: "mongodb://192.168.0.99:27017/resource"
+    /**
+     * 上传文件相关配置
+     */
+    uploadConfig: {
+        aliOss: {
+            internal: true,
         },
-    }
+        amzS3: {}
+    },
 }
