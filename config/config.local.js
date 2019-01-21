2e3f3189c427ef3a148f7db4d5af67ab07c96457
yuliang <yu.liang@freelog.com>
2019-01-21 16:54:43 +0800

    1.更新版本

--- config/config.local.js
+++ config/config.local.js
@@ -24,8 +24,24 @@ module.exports = {
         "updateDate": "2017-11-01T16:24:20.000Z"
     },
 
-    mongoose: {
-        url: "mongodb://127.0.0.1:27017/resource"
-    }
+    // mongoose: {
+    //     url: "mongodb://127.0.0.1:27017/resource"
+    // }
 
+    mongoose: {
+        url: "mongodb://119.23.45.143:27018/resource"
+    },
+
+    knex: {
+        //资源相关DB配置
+        resource: {
+            connection: {
+                host: '119.23.45.143',
+                user: 'root',
+                password: 'Ff@233109',
+                database: 'fr_resource',
+            },
+            debug: false
+        }
+    },
 }
