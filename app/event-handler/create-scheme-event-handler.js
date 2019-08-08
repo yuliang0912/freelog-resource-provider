f527e343fb9adbe0ac03151570cddd6701232f9b
yuliang <yu.liang@freelog.com>
2019-08-08 16:45:45 +0800

    1.修改BUG

--- app/event-handler/create-scheme-event-handler.js
+++ app/event-handler/create-scheme-event-handler.js
@@ -22,5 +22,43 @@ module.exports = class CreateSchemeEventHandler {
         await this.releaseProvider.findOneAndUpdate({_id: releaseInfo.id}, {
             $addToSet: {resourceVersions: latestVersion}, latestVersion
         })
+
+        if (!releaseInfo.resolveReleases.length) { // 未解决其他发行,不牵扯到合约问题,则直接发送事件
+            await this.sendSchemeCreatedEventToMessageQueue(releaseInfo, releaseSchemeInfo)
+        }
+    }
+
+    /**
+     * 发送方案创建事件
+     * @param releaseInfo
+     * @param releaseSchemeInfo
+     * @returns {Promise<*>}
+     */
+    async sendSchemeCreatedEventToMessageQueue(releaseInfo, releaseSchemeInfo) {
+
+        const ctx = this.app.createAnonymousContext()
+        const {releaseId, schemeId, resourceId, version} = releaseSchemeInfo
+
+        /**
+         * releaseAuthTree中versionRanges若为空数组,则代表实际未使用,
+         * 但是又作为依赖的发行的基础上抛(自己没上抛)必须解决.实际授权过程中针对此发行不做合约状态校验,但是需要把此情况反馈给授权系统.由授权系统做决策
+         * @type {Array|void}
+         */
+        const releaseAuthTree = await ctx.service.releaseService.releaseAuthTree(releaseInfo, {resourceId, version})
+
+        const params = {
+            routingKey: ReleaseSchemeCreatedEvent.routingKey,
+            eventName: ReleaseSchemeCreatedEvent.eventName,
+            body: {
+                releaseId, schemeId, resourceId, version,
+                resolveReleases: releaseAuthTree.map(item => Object({
+                    releaseId: item.releaseId,
+                    contracts: item.contracts.map(x => x.contractId),
+                    versionRanges: item.versionRanges
+                }))
+            }
+        }
+
+        return this.app.rabbitClient.publish(params)
     }
 }
