70b287e260b671e3a65876a8feb8d787331376cb
yuliang <yu.liang@freelog.com>
2020-01-02 14:15:05 +0800

    1.修改配置

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,13 +1,11 @@
 'use strict'
 
 module.exports = {
-
-    /**
-     * mongoDB配置
-     */
+    
     mongoose: {
-        url: "mongodb://172.18.215.231:27017/resource"
+        url: "mongodb://mongo-prod.common:27017/resource"
     },
+
 
     /**
      * 上传文件相关配置
