ea397f77645dba7aa3ab518282e399f523fed045
yuliang <4896819@qq.com>
2022-01-25 10:37:54 +0800

    1.提交代码

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,8 +1,12 @@
 export default () => {
     const config: any = {};
 
+    // config.mongoose = {
+    //     url: decodeURIComponent('mongodb%3A%2F%2Fresource_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-resources%3FreplicaSet%3Dmgset-58730021')
+    // };
+
     config.mongoose = {
-        url: decodeURIComponent('mongodb%3A%2F%2Fresource_service%3AQzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-resources%3FreplicaSet%3Dmgset-58730021')
+        url: decodeURIComponent('mongodb%3A%2F%2Froot%3AMTAwZGRhODU0Njc2MTM%3D%40freelog-prod-public.mongodb.rds.aliyuncs.com%3A3717%2Cfreelog-prod-public-secondary.mongodb.rds.aliyuncs.com%3A3717%2Fprod-resources%3FreplicaSet%3Dmgset-58730021')
     };
 
     config.cluster = {
